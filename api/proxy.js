dbc85031682a68190b92d182a301eeb21d12dcd8
AjnabiTricks <ajnabitricks@gmail.com>
2026-06-23 20:21:52 +0500

    Implement proxy function for external API requests
    Added a proxy function to handle requests and fetch data from an external API using axios.

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,37 @@
+const axios = require("axios");
 
+module.exports = async (req, res) => {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const cnic = req.query.cnic;
+
+  const url = "https://rodb.pulse.gop.pk/registry_index_3/_msearch";
+
+  const payload =
+`{"index":"registry_index_3"}
+{"query":{"bool":{"must":[
+{"term":{"TehsilId":{"value":"123"}}},
+{"nested":{
+  "path":"RegistryParties",
+  "query":{"bool":{"must":[
+    {"match":{"RegistryParties.CNIC":"${cnic}"}}
+  ]}}
+}}
+]}},"size":5}
+`;
+
+  try {
+    const response = await axios.post(url, payload + "\n", {
+      headers: {
+        "Content-Type": "application/x-ndjson",
+        "Authorization": "Basic cmVhZF9vbmx5X3VzZXJfdjI6cmVhZG9ubHlfMTIz"
+      }
+    });
+
+    res.status(200).json(response.data);
+
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
